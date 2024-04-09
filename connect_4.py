04c9c3ded8f3b04f67e81856270956a9ce4a526f
MChilly <mariatsili89@gmail.com>
2024-04-09 14:34:49 +0300

    added an images folder

--- connect_4.py
+++ connect_4.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 #  main program
 root = tk.Tk()
-root.iconbitmap(r"C:\Users\eirin\Documents\0.1.ΕΑΠ ΜΑΘΗΜΑΤΑ-ΠΛΗ\1.ΠΛΗΠΡΟ\Connect 4 project\other tests\game_dice.ico")
+root.iconbitmap(r"images\game_dice.ico")
 root.title = "Connect 4"
 root.geometry("800x800")
 
