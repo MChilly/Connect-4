78f9ce9f96b697e22616f2566fea88f89dedd84a
MChilly <mariatsili89@gmail.com>
2024-04-09 14:04:34 +0300

    First commit-just a mainloop window

---
+++ connect_4.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+#  main program
+root = tk.Tk()
+root.iconbitmap(r"C:\Users\eirin\Documents\0.1.ΕΑΠ ΜΑΘΗΜΑΤΑ-ΠΛΗ\1.ΠΛΗΠΡΟ\Connect 4 project\other tests\game_dice.ico")
+root.title = "Connect 4"
+root.geometry("800x800")
+
+root.mainloop()  # Αρχή βρόχου συμβάντων GUI
